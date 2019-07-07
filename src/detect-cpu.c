472acb8c80cdb2e012ef288aa9f7c363b09cc4f7
Oliver Cordes <ocordes@astro.uni-bonn.de>
2019-07-07 18:57:21 +0200

    - add some CPU features

--- src/detect-cpu.c
+++ src/detect-cpu.c
@@ -55,6 +55,115 @@ typedef struct {
 #define CPU_Vortex    13
 
 
+/* CPU feature flags */
+
+/*  Misc. */
+int HW_MMX;
+int HW_x64;
+int HW_ABM;      /* Advanced Bit Manipulation */
+int HW_RDRND;
+int HW_BMI;
+int HW_BMI2;
+int HW_RDSEED;
+int HW_ADX;
+int HW_CLFLUSHOPT;
+int HW_PREFETCHWT1;
+
+/*  SIMD: 128-bit */
+int HW_SSE;
+int HW_PCLMUL;
+int HW_SSE2;
+int HW_SSE3;
+int HW_SSSE3;
+int HW_SSE41;
+int HW_SSE42;
+int HW_SSE4a;
+int HW_AES;
+int HW_MOVBE;
+int HW_POPCNT;
+int HW_XSAVEC;
+int HW_XSAVES;
+int HW_F16C;
+int HW_SHA;
+
+/*  SIMD: 256-bit */
+int HW_FSGSBASE;
+int HW_AVX;
+int HW_XOP;
+int HW_FMA;
+int HW_FMA4;
+int HW_AVX2;
+
+/*  SIMD: 512-bit */
+int HW_AVX512F;    /*  AVX512 Foundation */
+int HW_AVX512CD;   /*  AVX512 Conflict Detection */
+int HW_AVX512PF;   /*  AVX512 Prefetch */
+int HW_AVX512ER;   /*  AVX512 Exponential + Reciprocal */
+int HW_AVX512VL;   /*  AVX512 Vector Length Extensions */
+int HW_AVX512BW;   /*  AVX512 Byte + Word */
+int HW_AVX512DQ;   /*  AVX512 Doubleword + Quadword */
+int HW_AVX512IFMA; /*  AVX512 Integer 52-bit Fused Multiply-Add */
+int HW_AVX512VBMI; /*  AVX512 Vector Byte Manipulation Instructions */
+
+
+/* Intel CPUs */
+
+typedef struct {
+  int id;
+  char *arch;
+} _cpu_arch;
+
+
+#define cpu_x86_64            0
+#define intel_core2           100
+#define intel_nehalem         101
+#define intel_westmere        102
+#define intel_sandybridge     103
+#define intel_ivybridge       104
+#define intel_haswell         105
+#define intel_broadwell       106
+#define intel_skylake         107
+#define intel_bonnell         108
+#define intel_silvermont      109
+#define intel_goldmont        110
+#define intel_goldmont_plus   111
+#define intel_tremont         112
+#define intel_knl             113
+#define intel_knm             114
+#define intel_skylake_avx512  115
+#define intel_cannonlake      116
+#define intel_icelake_client  117
+#define intel_icelake_server  118
+#define intel_cascadelake     119
+
+#define amd_athlon64          200
+
+
+_cpu_arch cpu_archs[] = {{intel_core2, "core2"},
+                         {intel_nehalem, "nehalem"},
+                         {intel_westmere, "westmere"},
+                         {intel_sandybridge, "sandybridge"},
+                         {intel_ivybridge, "ivybridge"},
+                         {intel_haswell, "haswell"},
+                         {intel_broadwell, "broadwell"},
+                         {intel_skylake, "skylake"},
+                         {intel_bonnell, "bonnell"},
+                         {intel_silvermont, "silvermont"},
+                         {intel_goldmont, "goldmont"},
+                         {intel_goldmont_plus, "goldmont-plus"},
+                         {intel_tremont, "tremont"},
+                         {intel_knl, "knl"},
+                         {intel_knm, "knm"},
+                         {intel_skylake_avx512, "skylake-avx512"},
+                         {intel_cannonlake, "cannonlake"},
+                         {intel_icelake_client, "icelake-client"},
+                         {intel_icelake_server, "icelake-server"},
+                         {intel_cascadelake, "cascadelake"},
+                         {amd_athlon64, "athlon64"},
+                         {cpu_x86_64, NULL}
+                       };
+
+
 int cpu_type;
 
 _vendor_strings vendor_string[14] = { { CPU_Intel, "GenuineIntel"},
@@ -116,45 +225,6 @@ void set_cpu_type(void)
   cpu_type = CPU_UNKNOWN;
 }
 
-/*  Misc. */
-int HW_MMX;
-int HW_x64;
-int HW_ABM;      /* Advanced Bit Manipulation */
-int HW_RDRAND;
-int HW_BMI1;
-int HW_BMI2;
-int HW_ADX;
-int HW_PREFETCHWT1;
-
-/*  SIMD: 128-bit */
-int HW_SSE;
-int HW_SSE2;
-int HW_SSE3;
-int HW_SSSE3;
-int HW_SSE41;
-int HW_SSE42;
-int HW_SSE4a;
-int HW_AES;
-int HW_SHA;
-
-/*  SIMD: 256-bit */
-int HW_AVX;
-int HW_XOP;
-int HW_FMA3;
-int HW_FMA4;
-int HW_AVX2;
-
-/*  SIMD: 512-bit */
-int HW_AVX512F;    /*  AVX512 Foundation */
-int HW_AVX512CD;   /*  AVX512 Conflict Detection */
-int HW_AVX512PF;   /*  AVX512 Prefetch */
-int HW_AVX512ER;   /*  AVX512 Exponential + Reciprocal */
-int HW_AVX512VL;   /*  AVX512 Vector Length Extensions */
-int HW_AVX512BW;   /*  AVX512 Byte + Word */
-int HW_AVX512DQ;   /*  AVX512 Doubleword + Quadword */
-int HW_AVX512IFMA; /*  AVX512 Integer 52-bit Fused Multiply-Add */
-int HW_AVX512VBMI; /*  AVX512 Vector Byte Manipulation Instructions */
-
 
 void get_cpu_flags(void)
 {
@@ -164,6 +234,8 @@ void get_cpu_flags(void)
 
   cpuid(info, 0);
   nIds = info[0];
+
+  printf("%x\n", nIds);
 
   /* read the vendor string */
   cpu_manufacturer_id(info[1], info[2], info[3]);
@@ -179,27 +251,33 @@ void get_cpu_flags(void)
     HW_MMX    = (info[3] & ((int)1 << 23)) != 0;
     HW_SSE    = (info[3] & ((int)1 << 25)) != 0;
     HW_SSE2   = (info[3] & ((int)1 << 26)) != 0;
-    HW_SSE3   = (info[2] & ((int)1 <<  0)) != 0;
 
+    HW_SSE3   = (info[2] & ((int)1 <<  0)) != 0;
+    HW_PCLMUL = (info[2] & ((int)1 <<  1)) != 0;
     HW_SSSE3  = (info[2] & ((int)1 <<  9)) != 0;
+    HW_FMA    = (info[2] & ((int)1 << 12)) != 0;
     HW_SSE41  = (info[2] & ((int)1 << 19)) != 0;
     HW_SSE42  = (info[2] & ((int)1 << 20)) != 0;
+    HW_MOVBE  = (info[2] & ((int)1 << 22)) != 0;
+    HW_POPCNT = (info[2] & ((int)1 << 23)) != 0;
     HW_AES    = (info[2] & ((int)1 << 25)) != 0;
-
+    HW_XSAVEC = (info[2] & ((int)1 << 26)) != 0;
+    HW_XSAVES = (info[2] & ((int)1 << 27)) != 0;
     HW_AVX    = (info[2] & ((int)1 << 28)) != 0;
-    HW_FMA3   = (info[2] & ((int)1 << 12)) != 0;
-
-    HW_RDRAND = (info[2] & ((int)1 << 30)) != 0;
+    HW_F16C   = (info[2] & ((int)1 << 29)) != 0;
+    HW_RDRND  = (info[2] & ((int)1 << 30)) != 0;
   }
 
   if (nIds >= 0x00000007)
   {
     cpuid(info,0x00000007);
-    HW_AVX2   = (info[1] & ((int)1 <<  5)) != 0;
-
-    HW_BMI1        = (info[1] & ((int)1 <<  3)) != 0;
+    HW_FSGSBASE    = (info[1] & ((int)1 <<  0)) != 0;
+    HW_BMI         = (info[1] & ((int)1 <<  3)) != 0;
+    HW_AVX2        = (info[1] & ((int)1 <<  5)) != 0;
     HW_BMI2        = (info[1] & ((int)1 <<  8)) != 0;
+    HW_RDSEED      = (info[1] & ((int)1 << 18)) != 0;
     HW_ADX         = (info[1] & ((int)1 << 19)) != 0;
+    HW_CLFLUSHOPT  = (info[1] & ((int)1 << 23)) != 0;
     HW_SHA         = (info[1] & ((int)1 << 29)) != 0;
     HW_PREFETCHWT1 = (info[2] & ((int)1 <<  0)) != 0;
 
@@ -226,9 +304,99 @@ void get_cpu_flags(void)
 }
 
 
+char *get_arch_name(int arch)
+{
+  int i;
+
+  i = 0;
+
+  while (cpu_archs[i].id != cpu_x86_64)
+  {
+    if (cpu_archs[i].id == arch)
+      return strdup(cpu_archs[i].arch);
+    ++i;
+  }
+
+  return strdup("x86-64");
+}
+
+
+
+int get_gcc_arch_type_intel(void)
+{
+  if (HW_MMX && HW_SSE && HW_SSE2 && HW_SSE3 && HW_SSSE3)
+  {
+    if (HW_SSE41 && HW_SSE42 && HW_POPCNT)
+    {
+      if (HW_AES && HW_PCLMUL)
+      {
+        if (HW_AVX)
+        {
+          if (HW_FSGSBASE && HW_RDRND && HW_F16C)
+          {
+            if (HW_MOVBE && HW_AVX2 && HW_FMA && HW_BMI && HW_BMI2)
+            {
+              if (HW_RDSEED && HW_ADX && HW_PREFETCHWT1 && HW_CLFLUSHOPT
+                  && HW_XSAVEC && HW_XSAVES)
+                return intel_skylake;
+              if (HW_RDSEED && HW_ADX && HW_PREFETCHWT1)
+                return intel_broadwell;
+
+              return intel_haswell;
+            }
+            return intel_ivybridge;
+          }
+          return intel_sandybridge;
+        }
+        return intel_westmere;
+      }
+      return intel_nehalem;
+    }
+
+    if (HW_MOVBE)
+      return intel_bonnell;
+      
+    return intel_core2;
+  }
+  else
+    return cpu_x86_64;
+}
+
+
+int get_gcc_arch_type(void)
+{
+  switch(cpu_type)
+  {
+    case CPU_Intel:
+        return get_gcc_arch_type_intel();
+        break;
+    case CPU_AMD:
+        return cpu_x86_64;
+        break;
+    default:
+        return cpu_x86_64;
+        break;
+  }
+}
+
+
 void report_cpu_data(void)
 {
   printf("vendor: %s\n", cpu_id_str);
+  printf("cpu_type=%i\n", cpu_type);
+}
+
+
+void cpu_arch_type(void)
+{
+  char *s;
+  int   arch;
+
+  arch = get_gcc_arch_type();
+
+  s = get_arch_name(arch);
+
+  printf("%s\n", s);
 }
 
 
@@ -237,6 +405,9 @@ int main(int argc, char* argv[])
   get_cpu_flags();
 
   report_cpu_data();
+
+
+  cpu_arch_type();
 
   return 0;
 }
